3d01a30b4ac22a1e9c4b5944c6d974d24c80531b
BytebyHimangi <himangi.awariya22@gmail.com>
2025-08-15 23:08:43 +0530

    initial commit

---
+++ Himexa2.0/src/context/UserContext.jsx
@@ -0,0 +1,51 @@
+import React, { createContext } from 'react'
+import run from '../gemini';
+export const datacontext=createContext();
+
+function UserContext({ children }) {
+let [speaking, setSpeaking] = React.useState(false);
+let [prompt,setPrompt] = React.useState("listening..");
+let [response, setResponse] = React.useState("");
+
+    function speak(text){
+let text_speak=new SpeechSynthesisUtterance(text);
+text_speak.volume=1;
+text_speak.rate=1;
+text_speak.pitch=1;
+text_speak.lang="hi-GB";
+window.speechSynthesis.speak(text_speak);
+    }
+async function aiResponse(prompt){
+    let text=await run(prompt)
+    setPrompt(text);
+    speak(text);
+    setResponse(text);
+    
+
+}
+let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let recognition = new speechRecognition()
+recognition.onresult=(e)=>{
+let currentIndex=e.resultIndex;
+let transcript=e.results[currentIndex][0].transcript;
+setPrompt(transcript);
+aiResponse(transcript);   
+}
+    let value = {
+     recognition,
+     speaking,
+     setSpeaking,
+     prompt,
+     setPrompt,
+     response,
+    }
+  return (
+    <div>
+    <datacontext.Provider value={value}>
+      {children}
+    </datacontext.Provider>
+    </div>
+  )
+}
+
+export default UserContext
---
+++ Himexa2.0/src/gemini.js
@@ -0,0 +1,35 @@
+let apiKey="AIzaSyD_0ER7-J4Qb1XzqWAKDXrBLv_jPio9x6c";
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash", 
+});
+
+const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 22,
+    responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+    const chatSession = model.startChat({
+        generationConfig,
+        history: [   
+        ],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    return result.response.text();
+}
+
+export default run;
